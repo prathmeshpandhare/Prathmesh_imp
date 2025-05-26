160779c76513bc0eadce81f0469149bbacffc772
prathameshBG-29 <prathmeshbugbattlers@gmail.com>
2025-05-26 19:00:17 +0530

    cors

--- Prathmesh-Backend/api/server.js
+++ Prathmesh-Backend/api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import ownersRoutes from "../routes/owners.js";
 import dealsRoutes from "../routes/deals.js";
@@ -8,23 +9,19 @@ import paymentRoutes from "../routes/paymentRoutes.js";
 import vanRoutes from "../routes/vanRoutes.js";
 import authRoutes from "../routes/authRoutes.js";
 import dashboardroutes from "../routes/dashboardRoutes.js";
+
 dotenv.config();
 const app = express();
-app.use(express.json());
 
-import cors from "cors";
-
-app.use(
-  cors({
-    origin: "http://localhost:5173", // your frontend URL
-    // You can add other options if needed
-  })
-);
+// Allow all origins
+app.use(cors());
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send({ activestaus: "active", message: "Welcome to the Van Rental API" });
 });
+
 // Your routes
 app.use("/api/owners", ownersRoutes);
 app.use("/api/deals", dealsRoutes);
