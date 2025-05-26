08f4140182935f15df325e717cf7c9183a754398
prathameshBG-29 <prathmeshbugbattlers@gmail.com>
2025-05-26 18:23:42 +0530

    new

--- Prathmesh-Backend/api/server.js
+++ Prathmesh-Backend/api/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import ownersRoutes from "./routes/owners.js";
-import dealsRoutes from "./routes/deals.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
-import vanRoutes from "./routes/vanRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import dashboardroutes from "./routes/dashboardRoutes.js";
+import ownersRoutes from "../routes/owners.js";
+import dealsRoutes from "../routes/deals.js";
+import paymentRoutes from "../routes/paymentRoutes.js";
+import vanRoutes from "../routes/vanRoutes.js";
+import authRoutes from "../routes/authRoutes.js";
+import dashboardroutes from "../routes/dashboardRoutes.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
